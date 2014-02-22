e92a73091a68796ec733e2ca8c7bf43ba13e4460
gmanolache <g@gmanolache.com>
2014-02-22 16:57:39 +0100

    commit changes

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,225 +1,229 @@
 angular.module('ionic.weather.directives', [])
 
-.constant('WEATHER_ICONS', {
-  'partlycloudy': 'ion-ios7-partlysunny-outline',
-  'mostlycloudy': 'ion-ios7-partlysunny-outline',
-  'cloudy': 'ion-ios7-cloudy-outline',
-  'rain': 'ion-ios7-rainy-outline',
-  'tstorms': 'ion-ios7-thunderstorm-outline',
-  'sunny': 'ion-ios7-sunny-outline',
-  'nt_clear': 'ion-ios7-moon-outline'
-})
+    .constant('WEATHER_ICONS', {
+        'tornado'					:'ion-ios7-thunderstorm',
+        'tropical storm'			:'ion-ios7-thunderstorm',
+        'hurricane'					:'ion-ios7-thunderstorm',
+        'severe thunderstorms'		:'ion-flash',
+        'thunderstorms'				:'ion-ios7-thunderstorm',
+        'mixed rain and snow'		:'ion-ios7-rainy',
+        'mixed rain and sleet'		:'ion-ios7-rainy',
+        'mixed snow and sleet'		:'ion-ios7-rainy',
+        'freezing drizzle'			:'ion-waterdrop',
+        'drizzle'					:'ion-waterdrop',
+        'freezing rain'				:'ion-ios7-rainy',
+        'showers'					:'ion-ios7-rainy',
+        'snow flurries'				:'ion-ios7-star-outline',
+        'light snow showers'		:'ion-ios7-star-outline',
+        'blowing snow'				:'ion-ios7-star-outline',
+        'snow'						:'ion-ios7-star-outline',
+        'hail'						:'ion-ios7-star-outline',
+        'sleet'						:'ion-ios7-rainy-outline',
+        'dust'						:'ion-ios7-rainy-outline',
+        'foggy'						:'ion-ios7-rainy-outline',
+        'haze'						:'ion-ios7-rainy-outline',
+        'smoky'						:'ion-ios7-rainy-outline',
+        'blustery'					:'ion-ios7-rainy-outline',
+        'windy'						:'ion-ios7-rainy-outline',
+        'cold'						:'ion-ios7-rainy-outline',
+        'cloudy'					:'ion-ios7-cloudy-outline',
+        'mostly cloudy (night)'		:'ion-cloud',
+        'mostly cloudy (day)'		:'ion-ios7-partlysunny-outline',
+        'partly cloudy (night)'		:'ion-cloud',
+        'partly cloudy (day)'		:'ion-ios7-partlysunny-outline',
+        'clear (night)'				:'ion-ios7-moon-outline',
+        'sunny'						:'ion-ios7-sunny-outline',
+        'fair (night)'				:'ion-ios7-moon',
+        'fair (day)'				:'ion-ios7',
+        'mixed rain and hail'		:'ion-ios7-rainy-outline',
+        'hot'						:'ion-ios7-sunny-outline',
+        'isolated thunderstorms'	:'ion-ios7-thunderstorm-outline',
+        'scattered thunderstorms'	:'ion-ios7-thunderstorm-outline',
+        'scattered thunderstorms'	:'ion-ios7-thunderstorm-outline',
+        'scattered showers'			:'ion-ios7-thunderstorm-outline',
+        'heavy snow'				:'ion-ios7-star-outline',
+        'scattered snow showers'	:'ion-ios7-star-outline',
+        'heavy snow'				:'ion-ios7-star-outline',
+        'partly cloudy'				:'ion-ios7-partlysunny-outline',
+        'thundershowers'			:'ion-ios7-thunderstorm',
+        'snow showers'				:'',
+        'isolated thundershowers'	:'ion-ios7-thunderstorm',
+        'not available'				:''
+    })
 
-.directive('weatherIcon', function(WEATHER_ICONS) {
-  return {
-    restrict: 'E',
-    replace: true,
-    scope: {
-      icon: '='
-    },
-    template: '<i class="icon" ng-class="weatherIcon"></i>',
-    link: function($scope) {
+    .directive('weatherIcon', function(WEATHER_ICONS) {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                icon: '='
+            },
+            template: '<i class="icon" ng-class="weatherIcon"></i>',
+            link: function($scope) {
 
-      $scope.$watch('icon', function(v) {
-        if(!v) { return; }
+                $scope.$watch('icon', function(v) {
+                    if(!v) { return; }
 
-        var icon = v;
+                    var icon = v;
 
-        if(icon in WEATHER_ICONS) {
-          $scope.weatherIcon = WEATHER_ICONS[icon];
-        } else {
-          $scope.weatherIcon = WEATHER_ICONS['cloudy'];
-        }
-      });
-    }
-  }
-})
-
-.directive('currentTime', function($timeout, $filter) {
-  return {
-    restrict: 'E',
-    replace: true,
-    template: '<span class="current-time">{{currentTime}}</span>',
-    scope: {
-      localtz: '=',
-    },
-    link: function($scope, $element, $attr) {
-      $timeout(function checkTime() {
-        if($scope.localtz) {
-          $scope.currentTime = $filter('date')(+(new Date), 'h:mm') + $scope.localtz;
-        }
-        $timeout(checkTime, 500);
-      });
-    }
-  }
- })
-
-.directive('currentWeather', function($timeout, $rootScope, Settings) {
-  return {
-    restrict: 'E',
-    replace: true,
-    templateUrl: 'templates/current-weather.html',
-    scope: true,
-    compile: function(element, attr) {
-      return function($scope, $element, $attr) {
-
-        $rootScope.$on('settings.changed', function(settings) {
-          var units = Settings.get('tempUnits');
-
-          if($scope.forecast) {
-
-            var forecast = $scope.forecast;
-            var current = $scope.current;
-
-            if(units == 'f') {
-              $scope.highTemp = forecast.forecastday[0].high.fahrenheit;
-              $scope.lowTemp = forecast.forecastday[0].low.fahrenheit;
-              $scope.currentTemp = Math.floor(current.temp_f);
-            } else {
-              $scope.highTemp = forecast.forecastday[0].high.celsius;
-              $scope.lowTemp = forecast.forecastday[0].low.celsius;
-              $scope.currentTemp = Math.floor(current.temp_c);
+                    if(icon in WEATHER_ICONS) {
+                        $scope.weatherIcon = WEATHER_ICONS[icon];
+                    } else {
+                        $scope.weatherIcon = WEATHER_ICONS['cloudy'];
+                    }
+                });
             }
-          }
-        });
-
-        $scope.$watch('current', function(current) {
-          var units = Settings.get('tempUnits');
-
-          if(current) {
-            if(units == 'f') {
-              $scope.currentTemp = Math.floor(current.temp_f);
-            } else {
-              $scope.currentTemp = Math.floor(current.temp_c);
-            }
-          }
-        });
-
-        $scope.$watch('forecast', function(forecast) {
-          var units = Settings.get('tempUnits');
-
-          if(forecast) {
-            if(units == 'f') {
-              $scope.highTemp = forecast.forecastday[0].high.fahrenheit;
-              $scope.lowTemp = forecast.forecastday[0].low.fahrenheit;
-            } else {
-              $scope.highTemp = forecast.forecastday[0].high.celsius;
-              $scope.lowTemp = forecast.forecastday[0].low.celsius;
-            }
-          }
-        });
-
-      // Delay so we are in the DOM and can calculate sizes
-      $timeout(function() {
-        var windowHeight = window.innerHeight;
-        var thisHeight = $element[0].offsetHeight;
-        var headerHeight = document.querySelector('#header').offsetHeight;
-        $element[0].style.paddingTop = (windowHeight - thisHeight) + 'px';
-        angular.element(document.querySelector('.content')).css('-webkit-overflow-scrolling', 'auto');
-        $timeout(function() {
-          angular.element(document.querySelector('.content')).css('-webkit-overflow-scrolling', 'touch');
-        }, 50);
-      });
-      }
-    }
-  }
-})
-
-.directive('forecast', function($timeout) {
-  return {
-    restrict: 'E',
-    replace: true,
-    templateUrl: 'templates/forecast.html',
-    link: function($scope, $element, $attr) {
-    }
-  }
-})
-
-.directive('weatherBox', function($timeout) {
-  return {
-    restrict: 'E',
-    replace: true,
-    transclude: true,
-    scope: {
-      title: '@'
-    },
-    template: '<div class="weather-box"><h4 class="title">{{title}}</h4><div ng-transclude></div></div>',
-    link: function($scope, $element, $attr) {
-    }
-  }
-})
-
-.directive('scrollEffects', function() {
-  return {
-    restrict: 'A',
-    link: function($scope, $element, $attr) {
-      var amt, st, header;
-      var bg = document.querySelector('.bg-image');
-      $element.bind('scroll', function(e) {
-        if(!header) {
-          header = document.getElementById('header');
         }
-        st = e.detail.scrollTop;
-        if(st >= 0) {
-          header.style.webkitTransform = 'translate3d(0, 0, 0)';
-        } else if(st < 0) {
-          header.style.webkitTransform = 'translate3d(0, ' + -st + 'px, 0)';
+    })
+
+    .directive('currentTime', function($timeout, $filter) {
+        return {
+            restrict: 'E',
+            replace: true,
+            template: '<span class="current-time">{{currentTime}}</span>',
+            scope: {
+                localtz: '='
+            },
+            link: function($scope, $element, $attr) {
+                $timeout(function checkTime() {
+                    if($scope.localtz) {
+                        $scope.currentTime = $filter('date')(+(new Date), 'h:mm') + $scope.localtz;
+                    }
+                    $timeout(checkTime, 500);
+                });
+            }
         }
-        amt = Math.min(0.6, st / 1000);
+    })
 
-        window.rAF(function() {
-          header.style.opacty = 1 - amt;
-          if(bg) {
-            bg.style.opacity = 1 - amt;
-          }
-        });
-      });
-    }
-  }
-})
+    .directive('currentWeather', function($timeout, $rootScope, Settings) {
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: 'templates/current-weather.html',
+            scope: true,
+            compile: function(element, attr) {
+                return function($scope, $element, $attr) {
 
-.directive('backgroundCycler', function($compile, $animate) {
-  var animate = function($scope, $element, newImageUrl) {
-    var child = $element.children()[0];
+                    $rootScope.$on('settings.changed', function(settings) {
+                        //should update
+                    });
 
-    var scope = $scope.$new();
-    scope.url = newImageUrl;
-    var img = $compile('<background-image></background-image>')(scope);
+                    $scope.$watch('results', function(results) {
+                        if(results) {
+                            $scope.currentTemp = results.item.condition.temp;
+                            $scope.highTemp = results.item.forecast[0].high;
+                            $scope.lowTemp = results.item.forecast[0].low;
+                        }
+                    });
 
-    $animate.enter(img, $element, null, function() {
-      console.log('Inserted');
+                    $timeout(function() {
+                        var windowHeight = window.innerHeight;
+                        var thisHeight = $element[0].offsetHeight;
+                        var headerHeight = document.querySelector('#header').offsetHeight;
+                        $element[0].style.paddingTop = (windowHeight - thisHeight) + 'px';
+                        angular.element(document.querySelector('.content')).css('-webkit-overflow-scrolling', 'auto');
+                        $timeout(function() {
+                            angular.element(document.querySelector('.content')).css('-webkit-overflow-scrolling', 'touch');
+                        }, 50);
+                    });
+                }
+            }
+        }
+    })
+
+    .directive('forecast', function($timeout) {
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: 'templates/forecast.html',
+            link: function($scope, $element, $attr) {
+            }
+        }
+    })
+
+    .directive('weatherBox', function($timeout) {
+        return {
+            restrict: 'E',
+            replace: true,
+            transclude: true,
+            scope: {
+                title: '@'
+            },
+            template: '<div class="weather-box"><h4 class="title">{{title}}</h4><div ng-transclude></div></div>',
+            link: function($scope, $element, $attr) {
+            }
+        }
+    })
+
+    .directive('scrollEffects', function() {
+        return {
+            restrict: 'A',
+            link: function($scope, $element, $attr) {
+                var amt, st, header;
+                var bg = document.querySelector('.bg-image');
+                $element.bind('scroll', function(e) {
+                    if(!header) {
+                        header = document.getElementById('header');
+                    }
+                    st = e.detail.scrollTop;
+                    if(st >= 0) {
+                        header.style.webkitTransform = 'translate3d(0, 0, 0)';
+                    } else if(st < 0) {
+                        header.style.webkitTransform = 'translate3d(0, ' + -st + 'px, 0)';
+                    }
+                    amt = Math.min(0.6, st / 1000);
+
+                    window.rAF(function() {
+                        header.style.opacty = 1 - amt;
+                        if(bg) {
+                            //bg.style.opacity = 1 - amt;
+                        }
+                    });
+                });
+            }
+        }
+    })
+
+    .directive('backgroundCycler', function($compile, $animate) {
+        var animate = function($scope, $element, newImageUrl) {
+            var child = $element.children()[0];
+
+            var scope = $scope.$new();
+            scope.url = newImageUrl;
+            var img = $compile('<background-image></background-image>')(scope);
+
+            $animate.enter(img, $element, null, function() {
+            });
+            if(child) {
+                $animate.leave(angular.element(child), function() {
+                });
+            }
+        };
+
+        return {
+            restrict: 'E',
+            link: function($scope, $element, $attr) {
+                $scope.$watch('activeBgImage', function(v) {
+                    if(!v) { return; }
+                    var item = v;
+                    var url = "http://farm"+ item.farm +".static.flickr.com/"+ item.server +"/"+ item.id +"_"+ item.secret + "_z.jpg";
+                    animate($scope, $element, url);
+                });
+            }
+        }
+    })
+
+    .directive('backgroundImage', function($compile, $animate) {
+        return {
+            restrict: 'E',
+            template: '<div class="bg-image"></div>',
+            replace: true,
+            scope: true,
+            link: function($scope, $element, $attr) {
+                $element[0].id="bg-image"
+                if($scope.url) {
+                    $element[0].style.backgroundImage = 'url(' + $scope.url + ')';
+                }
+            }
+        }
     });
-    if(child) {
-      $animate.leave(angular.element(child), function() {
-        console.log('Removed');
-      });
-    }
-  };
-
-  return {
-    restrict: 'E',
-    link: function($scope, $element, $attr) {
-      $scope.$watch('activeBgImage', function(v) {
-        if(!v) { return; }
-        console.log('Active bg image changed', v);
-        var item = v;
-        var url = "http://farm"+ item.farm +".static.flickr.com/"+ item.server +"/"+ item.id +"_"+ item.secret + "_z.jpg";
-        animate($scope, $element, url);
-      });
-    }
-  }
-})
-
-.directive('backgroundImage', function($compile, $animate) {
-  return {
-    restrict: 'E',
-    template: '<div class="bg-image"></div>',
-    replace: true,
-    scope: true,
-    link: function($scope, $element, $attr) {
-      if($scope.url) {
-        $element[0].style.backgroundImage = 'url(' + $scope.url + ')';
-      }
-    }
-  }
-});
--- www/js/services.js
+++ www/js/services.js
@@ -1,213 +1,192 @@
 angular.module('ionic.weather.services', ['ngResource'])
 
-.constant('DEFAULT_SETTINGS', {
-  'tempUnits': 'f'
-})
+    .constant('DEFAULT_SETTINGS', {
+        'tempUnits': 'f'
+    })
 
-.factory('Settings', function($rootScope, DEFAULT_SETTINGS) {
-  var _settings = {};
-  try {
-    _settings = JSON.parse(window.localStorage['settings']);
-  } catch(e) {
-  }
-
-  // Just in case we have new settings that need to be saved
-  _settings = angular.extend({}, DEFAULT_SETTINGS, _settings);
-
-  if(!_settings) {
-    window.localStorage['settings'] = JSON.stringify(_settings);
-  }
-
-  var obj = {
-    getSettings: function() {
-      return _settings;
-    },
-    // Save the settings to localStorage
-    save: function() {
-      window.localStorage['settings'] = JSON.stringify(_settings);
-      $rootScope.$broadcast('settings.changed', _settings);
-    },
-    // Get a settings val
-    get: function(k) {
-      return _settings[k];
-    },
-    // Set a settings val
-    set: function(k, v) {
-      _settings[k] = v;
-      this.save();
-    },
-
-    getTempUnits: function() {
-      return _settings['tempUnits'];
-    }
-  }
-
-  // Save the settings to be safe
-  obj.save();
-  return obj;
-})
-
-.factory('Geo', function($q) {
-  return {
-    reverseGeocode: function(lat, lng) {
-      var q = $q.defer();
-
-      var geocoder = new google.maps.Geocoder();
-      geocoder.geocode({
-        'latLng': new google.maps.LatLng(lat, lng)
-      }, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          console.log('Reverse', results);
-          if(results.length > 0) {
-            var r = results[0];
-            var a, types;
-            var parts = [];
-            var foundLocality = false;
-            var foundState = false;
-            for(var i = 0; i < r.address_components.length; i++) {
-              a = r.address_components[i];
-              types = a.types;
-              for(var j = 0; j < types.length; j++) {
-                if(!foundLocality && types[j] == 'locality') {
-                  foundLocality = true;
-                  parts.push(a.long_name);
-                } else if(!foundState && types[j] == 'administrative_area_level_1') {
-                  foundState = true;
-                  //parts.push(a.long_name);
-                }
-              }
-            }
-            console.log('Reverse', parts);
-            q.resolve(parts.join(' '));
-          }
-        } else {
-          console.log('reverse fail', results, status);
-          q.reject(results);
+    .factory('Settings', function($rootScope, DEFAULT_SETTINGS) {
+        var _settings = {};
+        try {
+            _settings = JSON.parse(window.localStorage['settings']);
+        } catch(e) {
         }
-      })
 
-      return q.promise;
-    },
-    getLocation: function() {
-      var q = $q.defer();
+        // Just in case we have new settings that need to be saved
+        _settings = angular.extend({}, DEFAULT_SETTINGS, _settings);
 
-      navigator.geolocation.getCurrentPosition(function(position) {
-        q.resolve(position);
-      }, function(error) {
-        q.reject(error);
-      });
+        if(!_settings) {
+            window.localStorage['settings'] = JSON.stringify(_settings);
+        }
 
-      return q.promise;
-    }
-  };
-})
+        var obj = {
+            getSettings: function() {
+                return _settings;
+            },
+            // Save the settings to localStorage
+            save: function() {
+                window.localStorage['settings'] = JSON.stringify(_settings);
+                $rootScope.$broadcast('settings.changed', _settings);
+            },
+            // Get a settings val
+            get: function(k) {
+                return _settings[k];
+            },
+            // Set a settings val
+            set: function(k, v) {
+                _settings[k] = v;
+                this.save();
+            },
 
-.factory('Flickr', function($q, $resource, FLICKR_API_KEY) {
-  var baseUrl = 'http://api.flickr.com/services/rest/'
+            getTempUnits: function() {
+                return _settings['tempUnits'];
+            }
+        }
+
+        // Save the settings to be safe
+        obj.save();
+        return obj;
+    })
+
+    .factory('Geo', function($q) {
+        return {
+            reverseGeocode: function(lat, lng) {
+                var q = $q.defer();
+
+                var geocoder = new google.maps.Geocoder();
+                geocoder.geocode({
+                    'latLng': new google.maps.LatLng(lat, lng)
+                }, function(results, status) {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        if(results.length > 0) {
+                            var r = results[0];
+                            var a, types;
+                            var parts = [];
+                            var foundLocality = false;
+                            var foundState = false;
+                            for(var i = 0; i < r.address_components.length; i++) {
+                                a = r.address_components[i];
+                                types = a.types;
+                                for(var j = 0; j < types.length; j++) {
+                                    if(!foundLocality && types[j] == 'locality') {
+                                        foundLocality = true;
+                                        parts.push(a.long_name);
+                                    } else if(!foundState && types[j] == 'administrative_area_level_1') {
+                                        foundState = true;
+                                        //parts.push(a.long_name);
+                                    }
+                                }
+                            }
+                            q.resolve(parts.join(' '));
+                        }
+                    } else {
+                        q.reject(results);
+                    }
+                })
+
+                return q.promise;
+            },
+            getLocation: function() {
+                var q = $q.defer();
+
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    q.resolve(position);
+                }, function(error) {
+                    q.reject(error);
+                });
+
+                return q.promise;
+            }
+        };
+    })
+
+    .factory('Flickr', function($q, $resource, FLICKR_API_KEY) {
+        var baseUrl = 'http://api.flickr.com/services/rest/'
 
 
-  var flickrSearch = $resource(baseUrl, {
-    method: 'flickr.groups.pools.getPhotos',
-    group_id: '1463451@N25',
-    safe_search: 1,
-    jsoncallback: 'JSON_CALLBACK',
-    api_key: FLICKR_API_KEY,
-    format: 'json'
-  }, {
-    get: {
-      method: 'JSONP'
-    }
-  });
+        var flickrSearch = $resource(baseUrl, {
+            method: 'flickr.groups.pools.getPhotos',
+            group_id: '1463451@N25',
+            safe_search: 1,
+            jsoncallback: 'JSON_CALLBACK',
+            api_key: FLICKR_API_KEY,
+            format: 'json'
+        }, {
+            get: {
+                method: 'JSONP'
+            }
+        });
 
-  return {
-    search: function(tags, lat, lng) {
-      var q = $q.defer();
-  
-      console.log('Searching flickr for tags', tags);
+        var flickrGetWoeid = $resource(baseUrl, {
+            method: 'flickr.places.findByLatLon',
+            jsoncallback: 'JSON_CALLBACK',
+            api_key: FLICKR_API_KEY,
+            format: 'json'
+        }, {
+            get: {
+                method: 'JSONP'
+            }
+        });
 
-      flickrSearch.get({
-        tags: tags,
-        lat: lat,
-        lng: lng
-      }, function(val) {
-        q.resolve(val);
-      }, function(httpResponse) {
-        q.reject(httpResponse);
-      });
+        return {
+            search: function(tags, lat, lng) {
+                var q = $q.defer();
 
-      return q.promise;
-    }
-  };
-})
+                flickrSearch.get({
+                    tags: tags,
+                    lat: lat,
+                    lng: lng
+                }, function(val) {
+                    q.resolve(val);
+                }, function(httpResponse) {
+                    q.reject(httpResponse);
+                });
 
-.factory('Weather', function($q, $resource, WUNDERGROUND_API_KEY) {
-  var baseUrl = 'http://api.wunderground.com/api/' + WUNDERGROUND_API_KEY;
+                return q.promise;
+            },
+            getWoeid: function(lat, lng) {
+                var q = $q.defer();
 
-  var locationResource = $resource(baseUrl + '/geolookup/conditions/q/:coords.json', {
-    callback: 'JSON_CALLBACK'
-  }, {
-    get: {
-      method: 'JSONP'
-    }
-  });
+                flickrGetWoeid.get({
+                    lat: lat,
+                    lon: lng
+                }, function(val) {
+                    q.resolve(val);
+                }, function(httpResponse) {
+                    q.reject(httpResponse);
+                });
 
-  var forecastResource = $resource(baseUrl + '/forecast/q/:coords.json', {
-    callback: 'JSON_CALLBACK'
-  }, {
-    get: {
-      method: 'JSONP'
-    }
-  });
+                return q.promise;
+            }
+        };
+    })
 
-  var hourlyResource = $resource(baseUrl + '/hourly/q/:coords.json', {
-    callback: 'JSON_CALLBACK'
-  }, {
-    get: {
-      method: 'JSONP'
-    }
-  });
 
-  return {
-    getForecast: function(lat, lng) {
-      var q = $q.defer();
+    .factory('YahooWeather', function($q, $resource) {
+        var locationUrl = "http://query.yahooapis.com/v1/public/yql?q=select%20item%20from%20weather.forecast%20where%20woeid%3D:woeid%20and%20u%3D%27:degrees%27&format=json&callback=JSON_CALLBACK";
 
-      forecastResource.get({
-        coords: lat + ',' + lng
-      }, function(resp) {
-        q.resolve(resp);
-      }, function(httpResponse) {
-        q.reject(httpResponse);
-      });
+        var forecastResource = $resource(locationUrl, {
+            jsoncallback: 'JSON_CALLBACK',
+            format: 'json'
+        }, {
+            get: {
+                method: 'JSONP'
+            }
+        });
 
-      return q.promise;
-    },
+        return {
+            getForecast: function(woeid, degrees) {
+                var q = $q.defer();
 
-    getHourly: function(lat, lng) {
-      var q = $q.defer();
+                forecastResource.get({
+                    woeid : woeid,
+                    degrees : degrees
+                }, function(resp) {
+                    q.resolve(resp);
+                }, function(httpResponse) {
+                    q.reject(httpResponse);
+                });
 
-      hourlyResource.get({
-        coords: lat + ',' + lng
-      }, function(resp) {
-        q.resolve(resp);
-      }, function(httpResponse) {
-        q.reject(httpResponse);
-      });
-
-      return q.promise;
-    },
-
-    getAtLocation: function(lat, lng) {
-      var q = $q.defer();
-
-      locationResource.get({
-        coords: lat + ',' + lng
-      }, function(resp) {
-        q.resolve(resp);
-      }, function(error) {
-        q.reject(error);
-      });
-
-      return q.promise;
-    }
-  }
-})
+                return q.promise;
+            }
+        }
+    })
